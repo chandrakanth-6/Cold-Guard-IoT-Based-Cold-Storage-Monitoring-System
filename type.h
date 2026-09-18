8921cb3e1480afeab902132b440a498e1c199b53
chandrakanth-6 <karidechandrakanth@gmail.com>
2026-09-18 19:14:30 +0530

    Add files via upload

---
+++ type.h
@@ -0,0 +1,9 @@
+typedef unsigned int u32;
+typedef const char cs8;
+typedef char s8;
+typedef int s32;
+typedef unsigned short int u16;
+typedef unsigned char u8;
+typedef const short int cu8;
+typedef float f32;
+
